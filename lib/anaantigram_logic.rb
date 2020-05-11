757c40ab4ed32c24364e17f137dc40004c9783b6
blacchoy@gmail.com <blacchoy@gmail.com>
2020-05-11 02:10:18 -0700

    wrote test to check if inputs are words

--- lib/anaantigram_logic.rb
+++ lib/anaantigram_logic.rb
@@ -6,6 +6,14 @@ attr_accessor(:word1, :word2)
     @word2 = word2
   end
 
+  def word_check()
+    if @word1.match?(/[aeiouy]/) & @word2.match?(/[aeiouy]/) 
+    return true
+  else
+    puts "Delete this and log off. These ain't words"
+    end
+  end
+
   def anagram_check ()
     @word1 = @word1.downcase.split('').sort.join('')
     @word2 = @word2.downcase.split('').sort.join('')
--- spec/anaantigram_logic_spec.rb
+++ spec/anaantigram_logic_spec.rb
@@ -3,14 +3,29 @@ require('anaantigram_logic')
 
 describe("Gram#anagram_check") do
   it ("Check if two words are anagrams") do
-    anagram = Gram.new("cat", "tac")
-  expect(anagram.anagram_check).to(eq(true))
+    user_words = Gram.new("cat", "tac")
+  expect(user_words.anagram_check).to(eq(true))
   end
 end
 
 describe("Gram#anagram_check") do
   it ("Check if two words are anagrams even if they are uppercase or lowercase") do
-    anagram1 = Gram.new("Cat", "tac")
-  expect(anagram1.anagram_check).to(eq(true))
+    user_words = Gram.new("Cat", "tac")
+  expect(user_words.anagram_check).to(eq(true))
   end
 end
+
+describe("Gram#word_check") do
+  it ("check if the inputs are words") do
+    user_words = Gram.new("act", "tac")
+  expect(user_words.word_check).to(eq(true))
+  end
+end
+
+describe("Gram#word_check") do
+  it ("check if the inputs are NOT words") do
+    user_words = Gram.new("zzjjjj", "hjjjh")
+  expect(user_words.word_check).to(eq(false))
+  end
+end
+
